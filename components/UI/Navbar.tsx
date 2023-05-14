ec741e31664568df48624f751569adbd01959413
Ali Abbas <maliabbas2001@gmail.com>
2023-05-15 03:30:12 +0500

    search bar implemented

--- components/UI/Navbar.tsx
+++ components/UI/Navbar.tsx
@@ -3,16 +3,10 @@ import Image from "next/image";
 import Link from "next/link";
 import { FaSearch } from "react-icons/fa";
 import Button from "./Button";
+import SearchBar from "./SearchBar";
 
 function Navbar() {
   const [menuOpen, setMenuOpen] = useState<boolean>(false);
-  const [searchInput, setSearchInput] = useState<string | null>("");
-
-  const handleSearch = (event: React.FormEvent<HTMLDivElement>) => {
-    event.preventDefault();
-    console.log(`Search for: ${searchInput}`);
-    setSearchInput("");
-  };
 
   const navbarItems = [
     {
@@ -52,35 +46,8 @@ function Navbar() {
             </a>
           </div>
 
-          <div className="hidden lg:ml-6 lg:flex lg:items-center">
-            {navbarItems.map((item, index) => (
-              <p
-                key={index}
-                className="px-3 py-2 text-gray-500 hover:text-gray-700"
-              >
-                <Link href={item.link}>{item.name}</Link>
-              </p>
-            ))}
-          </div>
-
-          <div className="flex items-center">
-            <div onSubmit={handleSearch} className="flex w-full lg:max-w-sm">
-              <form className="w-full flex lg:max-w-sm">
-                <input
-                  className="border rounded-l-lg p-2 w-full lg:w-auto text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                  type="text"
-                  placeholder="Search"
-                  value={searchInput}
-                  onChange={(e) => setSearchInput(e.target.value)}
-                />
-                <button
-                  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-r-lg focus:outline-none focus:shadow-outline"
-                  type="submit"
-                >
-                  <FaSearch />
-                </button>
-              </form>
-            </div>
+          <div className="flex items-center w-full max-w-xs md:max-w-sm">
+            <SearchBar />
 
             <button
               className="inline-flex items-center justify-center ml-4 p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 lg:hidden"
@@ -111,6 +78,17 @@ function Navbar() {
                 )}
               </svg>
             </button>
+          </div>
+
+          <div className="hidden lg:ml-6 lg:flex lg:items-center">
+            {navbarItems.map((item, index) => (
+              <p
+                key={index}
+                className="px-3 py-2 text-gray-500 hover:text-gray-700"
+              >
+                <Link href={item.link}>{item.name}</Link>
+              </p>
+            ))}
           </div>
 
           <div className=" lg:flex justify-between space-x-4 hidden ">
---
+++ components/UI/SearchBar.tsx
@@ -0,0 +1,99 @@
+import React, { useState } from "react";
+import { FaSearch } from "react-icons/fa";
+import { IoLocationSharp, IoCalendar } from "react-icons/io5";
+
+import events from "../../utils/all_events.json";
+import { EventType } from "../../types";
+import Image from "next/image";
+import Link from "next/link";
+
+const SearchBar = () => {
+  const [matchedEvents, setMatchedEvents] = useState<EventType[] | []>([]);
+  const [resultsFound, setResultsFound] = useState<boolean | null>(null);
+
+  const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const searched = e.target.value;
+
+    if (searched.length > 2) {
+      const matchedEvents = events.filter(
+        (event) =>
+          event.title.toLowerCase().includes(searched.toLowerCase()) ||
+          event.location.toLowerCase().includes(searched.toLowerCase()) ||
+          event.category.toLowerCase().includes(searched.toLowerCase())
+      );
+      matchedEvents.length > 0 ? setResultsFound(true) : setResultsFound(false);
+      setMatchedEvents(matchedEvents);
+    } else {
+      setMatchedEvents([]);
+      setResultsFound(null);
+    }
+  };
+
+  console.log(resultsFound);
+
+  return (
+    <div className="flex flex-col w-full">
+      <div onSubmit={null} className="flex w-full">
+        <form className="w-full flex flex-col relative">
+          <div className="flex">
+            <input
+              className="border rounded-l-lg p-2 w-full text-gray-500 font-medium leading-tight focus:outline-none focus:shadow-outline"
+              type="text"
+              placeholder="Search"
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                handleSearchChange(e)
+              }
+            />
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-r-lg focus:outline-none focus:shadow-outline"
+              type="submit"
+            >
+              <FaSearch />
+            </button>
+          </div>
+
+          {resultsFound === false && (
+            <div className="flex h-20 justify-center items-center text-lg text-gray-500 font-medium w-full absolute top-[51px] z-10 bg-white shadow-md rounded-lg shadow-gray-600">
+              <p>No Results Found</p>
+            </div>
+          )}
+
+          {matchedEvents.length > 0 && (
+            <div className="flex flex-col w-full absolute top-[51px] border border-b-0 z-10 bg-white shadow-lg shadow-gray-600">
+              {matchedEvents.slice(0, 5).map((event: EventType) => (
+                <Link key={event.id} href={`/event/${event.id}`}>
+                  <div className="flex w-full overflow-hidden space-x-4 items-center hover:bg-blue-100 transition-colors border-b p-2">
+                    <div className="w-20 h-20 relative">
+                      <Image
+                        src={event.image}
+                        alt={event.title}
+                        fill
+                        className="object-cover"
+                      />
+                    </div>
+
+                    <div className="text-slate-600 flex flex-col space-y-1 flex-1">
+                      <h3 className="text-xl font-medium">{event.title}</h3>
+
+                      <div className="flex flex-col md:flex-row w-full text-xs md:space-x-3">
+                        <div className="md:w-1/2">
+                          <p>{new Date(event.date).toDateString()}</p>
+                          <p>{event.location}</p>
+                        </div>
+                        <div className="md:w-1/2">
+                          <p className="uppercase">{event.category}</p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </Link>
+              ))}
+            </div>
+          )}
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default SearchBar;
