f22754663a2bf4c267964d54c9026420ef39bca5
Ali Abbas <maliabbas2001@gmail.com>
2023-05-27 17:48:41 +0500

    code fix

--- components/Profile/Profile.tsx
+++ components/Profile/Profile.tsx
@@ -12,7 +12,9 @@ const Profile = ({ name, email, phone, avatar }: UserType) => {
     <ComponentContainer title="User Profile">
       <div className="flex flex-col sm:flex-row sm:space-x-5 lg:space-x-10 sm:space-y-0 space-y-5 items-center sm:items-start">
         <div className="relative h-40 aspect-square">
-          <Image fill className="object-contain" alt={name} src={avatar} />
+          {avatar && (
+            <Image fill className="object-contain" alt={name} src={avatar}  />
+          )}
         </div>
         <div>
           <p className="text-2xl font-bold">{name}</p>
