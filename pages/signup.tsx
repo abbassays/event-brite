f1c512d38622adfd499e2371b785f2aa44df1a35
Ali Abbas <maliabbas2001@gmail.com>
2023-05-27 22:03:49 +0500

    all done

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -23,7 +23,7 @@ const signup = (props: Props) => {
         <div className="w-full bg-white rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
             <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
-              Create and account
+              Create an account
             </h1>
             <SignUpForm />
           </div>
