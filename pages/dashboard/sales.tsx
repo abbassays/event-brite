97f5f6f5421a92ea6cd3e86eb2044dd790c4ae6c
Ali Abbas <maliabbas2001@gmail.com>
2023-07-11 05:16:32 +0500

    sales filter added

--- pages/dashboard/sales.tsx
+++ pages/dashboard/sales.tsx
@@ -9,6 +9,8 @@ import AdminLayout from "@/components/CustomUI/AdminLayout";
 import Container from "@/components/CustomUI/Container";
 import Pagination from "@/components/CustomUI/Pagination";
 import SaleCard from "@/components/ListCards/SaleCard";
+import CustomSearchBar from "@/components/CustomUI/SearchBar";
+import { DatePickerWithRange } from "@/components/CustomUI/DateRangePicker";
 
 type Props = {};
 
@@ -37,6 +39,40 @@ const AllSalesPage = (props: Props) => {
     setVisibleSales(selectedTickets);
   };
 
+  const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (!e.target.value) {
+      setSales(allSales);
+      setVisibleSales(allSales.slice(0, itemsPerPage));
+      setCurrentPage(1);
+      return;
+    }
+
+    const searchedWord = e.target.value?.toLowerCase();
+    const searchedSales = allSales
+      .filter((sale) => {
+        return (
+          sale.organiserName
+            .toLowerCase()
+            .includes(searchedWord.toLowerCase()) ||
+          sale.eventName.toLowerCase().includes(searchedWord.toLowerCase())
+        );
+      })
+      .sort((a, b) => a.eventName.localeCompare(b.eventName));
+    setSales(searchedSales);
+    setVisibleSales(searchedSales.slice(0, itemsPerPage));
+    setCurrentPage(1);
+  };
+
+  const searchBar = (
+    <div className="flex flex-col sm:flex-row justify-end gap-4 items-end sm:items-center">
+      <DatePickerWithRange setSales={setSales} />
+      <CustomSearchBar
+        placeholder="Search by Event / Organiser"
+        onChange={(e) => handleSearch(e)}
+      />
+    </div>
+  );
+
   useEffect(() => initializeSales(), [allSales, customSession, selectedOrg]);
 
   useEffect(() => {
@@ -53,7 +89,7 @@ const AllSalesPage = (props: Props) => {
         title="All Sales"
         className="grid grid-cols-1 gap-2"
         gridItems={salesList}
-        // gridHeaders={searchBar}
+        gridHeaders={searchBar}
       >
         <Pagination
           currentPage={currentPage}
